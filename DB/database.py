83fe130b8d6f2b0633b241c62273fe53fe74b055
Vivek Prasad <Vivekpsd01@gmail.com>
2023-06-11 22:59:02 +0530

    DB CRUD

---
+++ DB/database.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+
+print("Opened database  Successfully")
+
+# conn.execute('''
+# create table todo (id int, task text, date text, status text)
+# ''')
+
+# print("Table Created!")
+
+# conn.execute('''
+# insert into todo (id, task, date, status) values (3, 'Jumping', '09/12/23', 'Done');
+# ''')
+# conn.commit()
+ 
+# print("Values inserted")
+
+# conn.execute('''
+# update todo set status = 'Done' where id = 1
+# ''')
+
+# conn.commit()
+
+conn.execute('''
+delete from todo where id=3
+''')
+conn.commit()
+
+result = conn.execute("select * from todo")
+
+for i in result:
+    print(i)
---
+++ GUI/Demo.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+calculation = ""
+
+root = tk.Tk()
+root.geometry("300x255")
+test_result = tk.Text(root, height=2, width=16)
+test_result.grid(columnspan=5)
+
+root.mainloop()
+
